69e39e939326c9f8ed747a3408cc8c75e54ad33b
AdanMedin <adanalonsomedin@gmail.com>
2023-11-14 18:20:25 +0100

    mosifico fichero test_operaciones.py para corregir un deprecationWarning en assertEqual y un error en el test de la división

--- test_operaciones.py
+++ test_operaciones.py
@@ -3,30 +3,30 @@ from operaciones import suma, resta, multiplicacion, division, raizCuadrada
 
 class TestOperaciones(unittest.TestCase):
     def test_s(self):
-        self.assertEquals(suma(4, 1), 5)
-        self.assertEquals(suma(-3, 1), -2)
-        self.assertEquals(suma(-2, 2), 0)
+        self.assertEqual(suma(4, 1), 5)
+        self.assertEqual(suma(-3, 1), -2)
+        self.assertEqual(suma(-2, 2), 0)
         
     def test_r(self):
-        self.assertEquals(resta(4, 1), 3)
-        self.assertEquals(resta(-3, 1), -4)
-        self.assertEquals(resta(2, -2), 4)
+        self.assertEqual(resta(4, 1), 3)
+        self.assertEqual(resta(-3, 1), -4)
+        self.assertEqual(resta(2, -2), 4)
         
     def test_m(self):
-        self.assertEquals(multiplicacion(4, 1), 4)
-        self.assertEquals(multiplicacion(-3, 0), 0)
-        self.assertEquals(multiplicacion(-2, 2), -4)
+        self.assertEqual(multiplicacion(4, 1), 4)
+        self.assertEqual(multiplicacion(-3, 0), 0)
+        self.assertEqual(multiplicacion(-2, 2), -4)
         
     def test_d(self):
-        self.assertEquals(division(4, 2), 2)
-        self.assertEquals(division(-3, 2), -1.5)
-        self.assertEquals(division(1, 2), 0.5)
-        with self.assertRaises(ValueError): # Comprovamos que falla si se intenta dividir un numero entre cero
+        self.assertEqual(division(4, 2), 2)
+        self.assertEqual(division(-3, 2), -1.5)
+        self.assertEqual(division(1, 2), 0.5)
+        with self.assertRaises(ZeroDivisionError): # Comprovamos que falla si se intenta dividir un numero entre cero
             division(4, 0)
         
     def test_sqrt(self):
-        self.assertEquals(raizCuadrada(0), 0)
-        self.assertEquals(raizCuadrada(3), 1.7320508075688772)
+        self.assertEqual(raizCuadrada(0), 0)
+        self.assertEqual(raizCuadrada(3), 1.7320508075688772)
         with self.assertRaises(ValueError): # Comprovamos que falla si se intenta obtener la raiz cuadrada de un numero negativo
             raizCuadrada(-2)
         
